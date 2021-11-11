19fb7a4250ddf364e2d94fa467f3ae86610a5130
Anna Seo <ebseo@Annas-MacBook.local>
2021-11-11 15:24:09 -0500

    Second Commit

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,23 +1,40 @@
 import React from 'react'
-import {Card, CardMedia, CardContent, CardActions, Typography, IconBotton} from '@material-ui/core';
-import {AddShoppingCart} from '@material-ui/icons'
-import classes from '*.module.css';
+import {Card, CardMedia, CardContent, CardActions, Typography, IconButton, Icon} from '@material-ui/core';
+import {AddShoppingCart} from '@material-ui/icons';
+// import classes from '*.module.css';
+import useStyles from './styles';
 
-const Product = () => {
+const Product = ({product}) => {
+    const classes = useStyles();
     return (
         <div>
             {/* adding classes */}
             <Card className={classes.root}>
                                                                     {/* this product.name is child component for Products.jsx */}
-                <CardMedia className={classes.media} image='' title={product.name}/>
+                <CardMedia className={classes.media} image={product.image} title={product.name}/>
                 <CardContent>
                     <div className={classes.CardContent}>
-
+                        <Typography variant="h5" gutterBottom>
+                            {product.name}
+                        </Typography>
+                        <Typography variant="h5" gutterBottom>
+                            {product.price}
+                        </Typography>
                     </div>
+                    <Typography variant="body2" color="textSecondary">{product.description}</Typography>
                 </CardContent>
+                <CardActions disableSpacing className={classes.CardActions}>
+                    <IconButton aria-label="Add to Cart">
+                        <AddShoppingCart/>
+                    </IconButton>
+                </CardActions>
             </Card>
         </div>
     )
 }
 
 export default Product
+
+
+// youtube: https://www.youtube.com/watch?v=377AQ0y6LPA
+// 6:39
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Grid} from '@material-ui/core';
+import Product from './Product/Product';
+
+const products =[
+    {id:1, name: 'Shoes', description: 'Running shoes', price: '$5', image: 'https://media.istockphoto.com/photos/pink-shoes-isolated-on-white-background-picture-id1168124796?k=20&m=1168124796&s=612x612&w=0&h=wfDhqA5yOuFuyfoHDikx78UWu_qyFnZWRZcrC58618E='},
+    {id:2, name: 'Macbook', description: 'Serise 9', price: '$59', image: 'https://media.istockphoto.com/photos/laptop-picture-id483912231?k=20&m=483912231&s=612x612&w=0&h=QkKZ4oEy9cd7605DeYnDpKilzrUZUybCoJ9k2cfwUzs='}
+];
+
+const Products = ()=> {
+    return(
+        <main>
+        <Grid container justify="center" spacing="{4}">
+            {products.map((product) => (
+                // for rooping in JSX, you need the id
+                                            //for devices' sizes
+                <Grid item key={products.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product product={product}/>
+                </Grid>
+            ))}
+        </Grid>
+    </main>
+    )
+}
+
+export default Products;
